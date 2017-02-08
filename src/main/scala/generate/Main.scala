e4d642580e189c17d91e1920910c5ee0bac5ff47
Paul Roberts <paul.roberts@guardian.co.uk>
2017-02-08 09:59:12 +0000

    write the generated scala code to a file

--- src/main/scala/generate/Main.scala
+++ src/main/scala/generate/Main.scala
@@ -1,6 +1,6 @@
 package com.gu.sangriascrooge.generate
 import org.scalafmt.Scalafmt
-import java.io.File
+import java.io.PrintStream
 import com.twitter.scrooge.frontend.{
   Importer,
   ThriftParser,
@@ -9,17 +9,14 @@ import com.twitter.scrooge.frontend.{
 
 object Main {
   def main(args: Array[String]) { 
+    val output = args.headOption.map(new PrintStream(_)).getOrElse(System.out)
     val paths = Option(System.getenv("THRIFT_PATHS")).getOrElse("example-thrift")
     val importer = Importer(paths.split(":"))
-    //val fname = "/home/proberts/.ivy2/cache/com.gu/content-atom-model-thrift/jars/content-atom-model-thrift-2.4.31.jar"
-    //val importer = new ZipImporter(new java.io.File(fname))
-    //val importer = new ResourceImporter() +: new ResourceImporter("/atoms/")
     val thriftParser = new ThriftParser(importer, false)
-    //val doc = thriftParser.parseFile("simple.thrift")
     val doc = thriftParser.parseFile("contentatom.thrift")
     val resolvedDoc = TypeResolver()(doc)
     val srcCode = (new SangriaScroogeGenerator).generate("ContentAtom", resolvedDoc.document)
-    println(
+    output.println(
       Scalafmt.format(srcCode.syntax).get
     )
   }
