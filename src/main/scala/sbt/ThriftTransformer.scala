ae9c25c48833004d365a2c56b013d7eb6bc5db71
Paul Roberts <paul.roberts@guardian.co.uk>
2017-02-13 15:19:36 +0000

    write generated source to a file

--- src/main/scala/sbt/ThriftTransformer.scala
+++ src/main/scala/sbt/ThriftTransformer.scala
@@ -6,6 +6,7 @@ package com.gu.thrifttransformer.sbt
 
 import com.gu.thrifttransformer.generate._
 import com.twitter.scrooge.frontend._
+import java.io.PrintStream
 
 import sbt._
 import Keys._
@@ -15,25 +16,40 @@ object ThriftTransformerSBT extends AutoPlugin {
     val thriftTransformPackageName = settingKey[String]("package to which the generated code should belong")
     val thriftTransformThriftDirs  = settingKey[Seq[File]]("directories to be search for thrift files")
     val thriftTransformThriftFiles = settingKey[Seq[File]]("files (from within the search path thriftTransformPackageName), from which code should be generated")
-
+    val thriftTransformSourceDir   = settingKey[File]("where will the generated source code be written to form part of the build?")
     val generateTransformedThrift  = taskKey[Seq[File]]("generate the requested code from the thrift file(s)")
   }
   import autoImport._
 
+  // sourceGenerators gets reset when Jvm Plugin starts, so we want to start
+  // after that
+  // <http://stackoverflow.com/questions/24724406/how-to-generate-sources-in-an-sbt-plugin?rq=1>
+  override lazy val requires = sbt.plugins.JvmPlugin
   override lazy val trigger = allRequirements
-  override lazy val requires = empty
   override lazy val projectSettings = Seq(
     thriftTransformPackageName := "thrift_transformed",
     thriftTransformThriftDirs  := Seq(baseDirectory.value / "src" / "main" / "thrift"),
     thriftTransformThriftFiles := Seq(file("set_in_scala_repo")),
+    thriftTransformSourceDir   := sourceManaged.value / "thriftTransform" / "src",
     generateTransformedThrift  := {
       val importer = Importer(thriftTransformThriftDirs.value.map(_.getCanonicalPath))
       val parser = new ThriftParser(importer, false)
       val resolver = new TypeResolver()
+      val generator = new CaseClassGenerator(Identifier(thriftTransformPackageName.value))
       val docs = thriftTransformThriftFiles.value.map { f =>
         resolver(parser.parseFile(f.getPath))
       }
-      Seq()
-    }
+      val srcFiles = docs.map(resolvedDoc => generator.generatePackage(resolvedDoc.document))
+      // write each document out to a file, returning the filename (as File())
+      srcFiles.map { srcFile =>
+        val fname = thriftTransformSourceDir.value / (thriftTransformPackageName.value + ".scala")
+        fname.getParentFile.mkdirs()
+        val out = new PrintStream(fname)
+        out.print(srcFile.generate)
+        out.close()
+        fname
+      }
+    },
+    sourceGenerators in Compile += generateTransformedThrift.taskValue
   )
 }
